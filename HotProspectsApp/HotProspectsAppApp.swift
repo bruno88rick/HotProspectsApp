050a2ed81e22cf2ac06fd8809c2cc5619e36ac8e
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-12-11 20:59:04 -0300

    Initial Commit

---
+++ HotProspectsApp/HotProspectsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsAppApp.swift
+//  HotProspectsApp
+//
+//  Created by Bruno Oliveira on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
